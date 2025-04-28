2375b2872e73870d2c25b9ee7ecba5bcdb3bcf95
5almiakki <jpui9635@gmail.com>
2025-04-28 22:03:28 +0900

    Create a new project

---
+++ open-ai-practice/src/main/java/kwc/openaipractice/OpenAiPracticeApplication.java
@@ -0,0 +1,13 @@
+package kwc.openaipractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAiPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenAiPracticeApplication.class, args);
+    }
+
+}
---
+++ open-ai-practice/src/test/java/kwc/openaipractice/OpenAiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package kwc.openaipractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAiPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
